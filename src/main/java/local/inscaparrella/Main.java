255da8e241a47f9404bb68d533fdc5f3579de717
Julia Carulla <jcarulla2@inscaparrella.cat>
2023-12-12 20:21:13 +0100

    branca test

--- src/main/java/local/inscaparrella/Main.java
+++ src/main/java/local/inscaparrella/Main.java
@@ -3,6 +3,7 @@ package local.inscaparrella;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hola a tots!");
         
     }
 }
