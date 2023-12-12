c87ad571fabfcd11c3b380798010b1f99a436797
Julia Carulla <jcarulla2@inscaparrella.cat>
2023-12-12 20:30:27 +0100

    actualitzar

--- src/main/java/local/inscaparrella/Main.java
+++ src/main/java/local/inscaparrella/Main.java
@@ -3,7 +3,7 @@ package local.inscaparrella;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hola local!");
+        System.out.println("Sóc la Júlia");
         
     }
 }
